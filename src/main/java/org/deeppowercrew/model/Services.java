493cd823ee531d88953c271105e9dd0f4c8171c8
DT5Gen <marinovod@gmail.com>
2024-10-03 02:00:35 +0300

    prebrush

---
+++ src/main/java/org/deeppowercrew/model/Services.java
@@ -0,0 +1,29 @@
+package org.deeppowercrew.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Services {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private ServiceType type;
+
+    private String description;
+
+    private String provider;
+
+    @ManyToOne
+    @JoinColumn(name = "attraction_id")
+    private Landmark landmark;
+}
---
+++ src/main/java/org/deeppowercrew/repository/LandmarkRepository.java
@@ -0,0 +1,9 @@
+package org.deeppowercrew.repository;
+
+import org.deeppowercrew.model.Landmark;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LandmarkRepository extends JpaRepository<Landmark, Long> {
+}
